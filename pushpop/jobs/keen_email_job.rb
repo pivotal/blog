1d1c4362a675f3b2583d46e4486ca931afe54a88
Tammer Saleh <me@tammersaleh.com>
2016-04-25 15:56:00 +0100

    Add style and more data

--- pushpop/jobs/keen_email_job.rb
+++ pushpop/jobs/keen_email_job.rb
@@ -1,4 +1,4 @@
-require 'json'
+require "json"
 
 svs = JSON.load(ENV["VCAP_SERVICES"])
 
@@ -8,28 +8,55 @@ ENV["SENDGRID_PASSWORD"] = svs["sendgrid"][0]["credentials"]["password"]
 ENV["KEEN_PROJECT_ID"]   = svs["user-provided"][0]["credentials"]["project_id"]
 ENV["KEEN_READ_KEY"]     = svs["user-provided"][0]["credentials"]["read_key"]
 
-require 'pushpop-keen'
-require 'pushpop-sendgrid'
+require "pushpop-keen"
+require "pushpop-sendgrid"
+require "roadie"
 
 # Define our job and name it
-job 'keen email' do
+job "keen email" do
 
-  every 24.hours, at: '00:00'
-  # every 1.hours, at: '**:00'
+  every 24.hours, at: "00:00"
+  # every 1.hours, at: "**:00"
 
-  step "page-loads-by-url" do
-    Keen.count('Loaded a Page', timeframe: 'last_30_days', group_by:  'parsed_page_url.path')
+  step do
+    Keen.count("Loaded a Page", timeframe: "last_30_days", group_by: "title", filters: [{
+        "property_name" => "parsed_page_url.path",
+        "operator" => "contains",
+        "property_value" => "/post/"
+      }
+    ])
+  end
+
+  step "top-pages" do |response, step_responses|
+    response.sort do |this, that|
+      that["result"].to_i <=> this["result"].to_i
+    end.first(10)
   end
 
   step "traffic-over-last-30-days" do
-    Keen.count('Loaded a Page', timeframe: 'last_30_days')
+    Keen.count("Loaded a Page", timeframe: "last_30_days")
   end
 
-  sendgrid do |response, step_responses|
-    to        "tsaleh@pivotal.io"
-    from      "tsaleh@pivotal.io"
-    subject   "Pivotal Eng Blog daily hit count"
-    body      template('email.html.erb', response, step_responses)
+  step "referrers" do
+    Keen.count("Loaded a Page", timeframe: "last_30_days", group_by: "referrer_info.source").sort do |this, that|
+      that["result"].to_i <=> this["result"].to_i
+    end.first(10)
+  end
+
+  if ENV["LOCAL"]
+    step "write-to-file" do |response, step_responses|
+      open("/tmp/keen-#{$$}.html", "w") do |f|
+        f.puts(Roadie::Document.new(template("email.html.erb", response, step_responses)).transform)
+      end
+      `open /tmp/keen-#{$$}.html`
+    end
+  else
+    sendgrid do |response, step_responses|
+      to        "Tammer Saleh <tsaleh@pivotal.io>"
+      from      "Blog Stats <stats@engineering.pivotal.io>"
+      subject   "Engineering Blog Traffic Report"
+      body      Roadie::Document.new(template("email.html.erb", response, step_responses)).transform
+    end
   end
 
 end
