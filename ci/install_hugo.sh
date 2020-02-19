33613806cd7e34391c56fa0783ae49f50c87e093
Dwayne Forde <dwayne.forde@gmail.com>
2020-02-19 17:20:29 -0500

    Upgrades Hugo 0.60.1 -> 0.64.1

--- ci/install_hugo.sh
+++ ci/install_hugo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.60.1
+VERSION=0.64.1
 
 mkdir /opt/hugo && cd /opt/hugo
 wget https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz
